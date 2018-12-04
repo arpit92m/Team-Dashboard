f5fe5e5c18e64f711e10295715fe1b129a185d4d
Apple <apple@Apples-MacBook-Pro.local>
2018-12-04 09:27:36 +0530

    added build

---
+++ build/precache-manifest.e37283c4c2585a9bb382b70ca807f043.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca09bc6b9b244e941b1a",
+    "url": "/static/js/main.ca09bc6b.chunk.js"
+  },
+  {
+    "revision": "e7f87026231080fede42",
+    "url": "/static/js/1.e7f87026.chunk.js"
+  },
+  {
+    "revision": "ca09bc6b9b244e941b1a",
+    "url": "/static/css/main.965bf72c.chunk.css"
+  },
+  {
+    "revision": "5553ebe513a7398b69ab79a9f245e4a8",
+    "url": "/index.html"
+  }
+];
